155c19828fdeecdd661028e45f49e4fc89b63661
ugo <ugo.gosso@gmail.com>
2024-11-23 23:54:17 +0100

    add searching strategy

--- src/main/kotlin/Action.kt
+++ src/main/kotlin/Action.kt
@@ -4,15 +4,54 @@ import java.io.File
 
 
 fun search(inputList: ArrayList<String>, inputMap: Map<String, List<Int>>) {
-    println("Enter data to search people:")
-    val toSearch = readln()
+    println("""
+        === Possible search strategy ===
+        ANY - prints the lines containing at least one word from the query  
+        ALL - prints the lines containing all the word from the query 
+        NONE - prints all the lines not containing the words from the query 
+        """.trimIndent())
 
-    if (toSearch.lowercase() in inputMap) {
-        for (i in inputMap[toSearch]!!) {
-            println(inputList[i])
+    val nextAction = readln()
+    if (!(nextAction.uppercase() == "ANY" || nextAction.uppercase() == "ALL" || nextAction.uppercase() == "NONE")) {
+        println("You need to enter a a valid option: ANY, ALL or NONE")
+        search(inputList, inputMap)
+    }
+
+    println("Enter data to search people:")
+    val toSearch = readln().split(" ")
+
+    if (nextAction.uppercase() == "ANY") {
+        var found = false
+        for (query in toSearch) {
+            if (query.lowercase() in inputMap) {
+                found = true
+                for (i in inputMap[query.lowercase()]!!) {
+                    println(inputList[i])
+                }
+            }
+        }
+
+        if (!found) {
+            println("No query found")
         }
     } else {
-        println("No matching people found.")
+        var found: Set<Int> = (0..<inputList.size).toSet()
+        for (query in toSearch) {
+            if (query.lowercase() in inputMap) {
+                found = found intersect inputMap[query.lowercase()]!!.toSet()
+                println(found)
+            }
+        }
+
+        if (nextAction.uppercase() == "ALL") {
+            for (i in found) {
+                println(inputList[i])
+            }
+        } else {
+            for (i in ((0..<inputList.size) - found)) {
+                println(inputList[i])
+            }
+        }
     }
 }
 
